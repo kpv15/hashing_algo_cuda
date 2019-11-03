9c1462e067d1821ba9eb0c5ac9bf4d749cb9b86f
kpv16 <kpv135@gmail.com>
2019-11-03 15:52:24 +0100

    first clion working version

---
+++ cuda_clion_hack.hpp
@@ -0,0 +1,13 @@
+//
+// Created by grzegorz on 03.11.2019.
+//
+
+#ifndef INZYNIERKA_CUDA_CLION_HACK_HPP
+#define INZYNIERKA_CUDA_CLION_HACK_HPP
+
+//include files for clion
+
+#include <device_launch_parameters.h>
+#include <cuda_runtime.h>
+
+#endif //INZYNIERKA_CUDA_CLION_HACK_HPP
