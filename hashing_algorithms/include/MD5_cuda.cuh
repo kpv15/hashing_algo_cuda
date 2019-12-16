392a473a3131e66ca0eca0f4045d3e9ba6a1ee87
Grzegorz Król <kpv135@gmail.com>
2019-12-16 20:56:03 +0100

    implement function f/g/h/i

--- hashing_algorithms/include/MD5_cuda.cuh
+++ hashing_algorithms/include/MD5_cuda.cuh
@@ -10,45 +10,35 @@
 
 class MD5_cuda : public IHashingAlgorithm {
 
+    struct block {
+        unsigned int a;
+        unsigned int b;
+        unsigned int c;
+        unsigned int d;
+    };
+
     const unsigned int DIGEST_LENGTH = 16;
+    static const unsigned char S[64];
+    static const block DEFAULT_DIGEST_BUFFER;
+    static const unsigned int K[64];
 
     unsigned long int defaultWordLength = 0;
     unsigned long int workingBufferLength = 0;
     unsigned long int numberOfChunks = 0;
     unsigned char *workingBuffer = nullptr;
-    unsigned char *digestBuffer = nullptr;
-    constexpr static unsigned char S[64] = {
-            7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
-            5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20,
-            4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
-            6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21
-    };
-    static const unsigned int DEFAULT_DIGEST_BUFFER[4];
-
-    constexpr static unsigned int K[64] = {
-            0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
-            0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
-            0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
-            0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
-            0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
-            0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
-            0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
-            0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
-            0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
-            0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
-            0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
-            0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
-            0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
-            0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
-            0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
-            0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
-    };
+    block mdBuffer;
 
     unsigned int calculateWorkingBufferLength();
 
     void createWorkingBuffer(const char *word);
 
-    void createDigestBuffer();
+    unsigned int functionF(const unsigned int x, const unsigned int y, const unsigned int z);
+
+    unsigned int functionG(const unsigned int x, const unsigned int y, const unsigned int z);
+
+    unsigned int functionH(const unsigned int x, const unsigned int y, const unsigned int z);
+
+    unsigned int functionI(const unsigned int x, const unsigned int y, const unsigned int z);
 
 public:
     void setDefaultWordLength(unsigned int i) override;
@@ -57,18 +47,41 @@ public:
 
     unsigned char *calculateHashSum(const char *word) override {
         createWorkingBuffer(word);
-        createDigestBuffer();
+        mdBuffer = DEFAULT_DIGEST_BUFFER;
 
-        int F, g;
+        unsigned int *chunks = reinterpret_cast<unsigned int *>(workingBuffer);
+
         for (unsigned int i = 0; i < numberOfChunks; i++) {
+            block stepBuffer = mdBuffer;
+            int F, g;
             for (int step = 0; step < 64; step++) {
-
-
+                if (step < 16) {
+                    F = functionF(st);
+                    g = i;
+                } else if (step < 32) {
+                    F = functionG(stepBuffer);
+                    g = (5 * i + 1) % 16;
+                } else if (step < 48) {
+                    F = functionH(stepBuffer);
+                    g = (3 * i + 5) % 16;
+                } else if (step < 64) {
+                    F = functionI(stepBuffer);
+                    g = (7 * i) % 16;
+                }
+                F += stepBuffer.a + K[i] + chunks[4 * i + g];
+                stepBuffer.a = stepBuffer.d;
+                stepBuffer.d = stepBuffer.c;
+                stepBuffer.c = stepBuffer.b;
+                stepBuffer.b += F << S[i];
             }
+            mdBuffer.a += stepBuffer.a;
+            mdBuffer.b += stepBuffer.b;
+            mdBuffer.c += stepBuffer.c;
+            mdBuffer.d += stepBuffer.d;
         }
 
-        unsigned char *toReturn = digestBuffer;
-        digestBuffer = nullptr;
+        unsigned char *toReturn = new unsigned char[DIGEST_LENGTH];
+        memcpy(toReturn, &mdBuffer, DIGEST_LENGTH);
         return toReturn;
     };
 
